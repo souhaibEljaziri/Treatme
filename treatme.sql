45f9b80a2e88224447e4ac1c10d6ad24960b9e55
ahmedTouati <ahmed75touati@gmail.com>
2021-07-24 23:49:37 +0200

    update BD Dump

--- treatme.sql
+++ treatme.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2021 at 11:23 PM
+-- Generation Time: Jul 24, 2021 at 11:48 PM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `treatme`
+-- Database: `tt`
 --
 
 -- --------------------------------------------------------
@@ -29,16 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `appointment` (
   `id` int(11) NOT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
+  `patient_id` int(11) DEFAULT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `start_date` datetime NOT NULL,
-  `end_date` datetime NOT NULL,
-  `repeat_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `startdate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `enddate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `repeattime` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `symptom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `doctor_id` int(11) NOT NULL,
-  `patient_id` int(11) NOT NULL
+  `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `doctor_id`, `patient_id`, `title`, `startdate`, `enddate`, `repeattime`, `symptom`, `location`) VALUES
+(3, 3, 1, 'string', 'string', 'string', 'string', 'string', 'string'),
+(5, 3, 2, 'wissem', 'wissem', 'wissem', 'wissem', 'wissem', 'wissem');
 
 -- --------------------------------------------------------
 
@@ -48,17 +55,45 @@ CREATE TABLE `appointment` (
 
 CREATE TABLE `doctor` (
   `id` int(11) NOT NULL,
-  `doctor_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doctorname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mobile_phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobilephone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `department` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `departement` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `education` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `experience` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `designation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `duty_timing` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `specialisation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `dutytiming` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `specilaisation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `doctorname`, `gender`, `mobilephone`, `email`, `departement`, `education`, `experience`, `designation`, `dutytiming`, `specilaisation`) VALUES
+(1, 'amjed', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string'),
+(3, 'najwa', 'women', '222', 'na', 'na', 'na', 'na', 'na', 'na', 'na');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20210717232354', '2021-07-18 01:24:03', 240),
+('DoctrineMigrations\\Version20210717233413', '2021-07-18 01:34:34', 38);
 
 -- --------------------------------------------------------
 
@@ -84,12 +119,22 @@ CREATE TABLE `notification` (
 
 CREATE TABLE `oxygene` (
   `id` int(11) NOT NULL,
-  `supplier_id` int(11) NOT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
   `water_capcity` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `oxygene_capacity` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oxygene`
+--
+
+INSERT INTO `oxygene` (`id`, `supplier_id`, `water_capcity`, `oxygene_capacity`, `status`, `price`) VALUES
+(1, 1, '100', '500', 'Available', '200'),
+(2, 2, '200', '5000', 'Sold', '500'),
+(3, 2, '100', '500', 'Sold', '500'),
+(4, 1, '200', '300', 'Sold', '200');
 
 -- --------------------------------------------------------
 
@@ -103,6 +148,14 @@ CREATE TABLE `oxygen_supplier` (
   `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oxygen_supplier`
+--
+
+INSERT INTO `oxygen_supplier` (`id`, `name`, `contact`, `location`) VALUES
+(1, 'Ali', '55200541', 'Bizerte'),
+(2, 'Monji', '25524885', 'Tunis');
 
 -- --------------------------------------------------------
 
@@ -119,6 +172,45 @@ CREATE TABLE `patient` (
   `mobile_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `patient_name`, `gender`, `date_of_birth`, `blood_group`, `mobile_number`, `email`) VALUES
+(1, 'Saleh', 'Male', '20/05/1997', 'A+', '97505836', 'Saleh@xxx.com'),
+(2, 'Farah', 'Female', '15/07/1995', 'B+', '25365898', 'Farah@xxx.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `oxygen_id` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `price` double DEFAULT NULL,
+  `tax` int(11) DEFAULT NULL,
+  `total` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `patient_id`, `supplier_id`, `oxygen_id`, `created_at`, `price`, `tax`, `total`) VALUES
+(1, 2, 1, 1, '2021-07-18', 100, 10, 110),
+(2, 1, 2, 2, '2021-07-14', 50, 10, 55),
+(22, 2, 1, 1, '2021-07-21', 100, 50, 150),
+(23, 2, 2, 2, '2021-07-21', 100, 0, 100),
+(24, 2, 1, 1, '2021-07-21', 100, 20, 120),
+(25, 1, 1, 4, '2021-07-24', 100, 30, 130),
+(26, 2, 2, 2, '2021-07-24', 100, 20, 120),
+(27, 2, 2, 3, '2021-07-24', 100, 20, 120);
 
 -- --------------------------------------------------------
 
@@ -157,6 +249,12 @@ ALTER TABLE `doctor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
 -- Indexes for table `notification`
 --
 ALTER TABLE `notification`
@@ -182,6 +280,15 @@ ALTER TABLE `patient`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_65D29B326B899279` (`patient_id`),
+  ADD KEY `IDX_65D29B322ADD6D8C` (`supplier_id`),
+  ADD KEY `IDX_65D29B32654C1ECB` (`oxygen_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -195,13 +302,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `notification`
@@ -213,19 +320,25 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `oxygene`
 --
 ALTER TABLE `oxygene`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `oxygen_supplier`
 --
 ALTER TABLE `oxygen_supplier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -249,6 +362,14 @@ ALTER TABLE `appointment`
 --
 ALTER TABLE `oxygene`
   ADD CONSTRAINT `FK_DD6AFBEF2ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `oxygen_supplier` (`id`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `FK_65D29B322ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `oxygen_supplier` (`id`),
+  ADD CONSTRAINT `FK_65D29B32654C1ECB` FOREIGN KEY (`oxygen_id`) REFERENCES `oxygene` (`id`),
+  ADD CONSTRAINT `FK_65D29B326B899279` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
